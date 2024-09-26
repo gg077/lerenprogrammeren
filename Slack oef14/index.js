16109e47127763a23c365128571e91cc7581b0d8
JanVDB2000 <janvandenbogaert2000@gmail.com>
2024-09-26 15:37:17 +0200

    Slack oefeningen

--- Slack oef14/index.js
+++ Slack oef14/index.js
@@ -1,17 +1,9 @@
-//let getal = parseInt(prompt("Geef een nummer"));
-//let totaal = 0;
+let getal = parseInt(prompt("Geef een nummer"));
+let totaal = 0;
 
-//for (let i = 1; i <= 100; i++) { // Begin i bij 1 en verhoog met 1
-//    if (getal * i <= 100) { // Controleer of het veelvoud ≤ 100 is
-//        totaal = getal * i; // Bereken het veelvoud
-//        console.log(totaal); // Print het veelvoud
-//    }
-//}
-
-let getal = parseInt(prompt("geef een nmr"))
-for (let i : 1;i<=200;i++){
-    if (getal >= 200){
-        totaal = getal * i
+for (let i = 1; i <= 100; i++) { // Begin i bij 1 en verhoog met 1
+    if (getal * i <= 100) { // Controleer of het veelvoud ≤ 100 is
+        totaal = getal * i; // Bereken het veelvoud
+        console.log(totaal); // Print het veelvoud
     }
 }
-
